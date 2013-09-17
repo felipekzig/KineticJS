2cb45232395afc0c0ff1aff0fce53223b94eeaa1
Felipe Klein Zigmundo <felipe.zigmundo@ecore.com.br>
2013-09-17 10:16:33 -0300

    Added a watch task to grunt.

--- Gruntfile.js
+++ Gruntfile.js
@@ -161,6 +161,10 @@ module.exports = function(grunt) {
         laxbreak: true
       },
       all: ['src/**/*.js']
+    },
+    watch: {
+      files: sourceFiles.concat(['test/unit/**/*.js']),
+      tasks: ['clean', 'concat:dev', 'replace:dev', 'jshint']
     }
   };
 
@@ -181,6 +185,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Tasks
   grunt.registerTask('dev', ['clean', 'concat:dev', 'replace:dev']);
